a399924b9dceb8ea1f45653e2ac2922d62d46e26
JL <JL@Dell>
2017-05-23 16:55:50 +0200

    xtienda

--- js/fcesta.js
+++ js/fcesta.js
@@ -1,13 +1,25 @@
+/**
+ * actualiza la cesta de la compra
+ * @return {undefined}
+ */
 function actualiza(){
       xajax_actualiza(xajax.getFormValues('listado'));
 };
+/**
+ * Vacia la cesta de la compra
+ * @return {undefined}
+ */
 function vaciarCesta(){
    // alert('en fcesta.js vaciando');
     xajax_vaciarCesta();
 }
 
-
+/**
+ * Borra un producto de la cesta
+ * @param {type} codigo
+ * @return {undefined}
+ */
 function borrarProducto(codigo) {
-    alert("borrando el producto "+codigo+" desde fcesta.js");
+   // alert("borrando el producto "+codigo+" desde fcesta.js");
     xajax_borrarProducto(codigo);
 }
